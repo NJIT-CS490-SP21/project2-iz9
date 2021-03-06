7b790a04c5498c2fad56a1e5c3758bedbe3a4962
EC2 Default User <ec2-user@ip-172-31-2-160.ec2.internal>
2021-03-06 21:36:07 +0000

    deleted comment not needed

--- models.py
+++ models.py
@@ -1,15 +1,3 @@
-
-'''
-def define_user_class(db):
-    class User(db.Model):
-        username = db.Column(db.String(80), primary_key=True)
-        score = db.Column(db.Integer, nullable=False)
-        
-        def __repr__(self):
-            return '<User %r>' % self.username
-    return User
-'''
-
 from app import db
 
 class Users(db.Model):

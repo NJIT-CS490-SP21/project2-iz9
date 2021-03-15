f39025c443ae8af6624392edd31393d979e776a7
EC2 Default User <ec2-user@ip-172-31-2-160.ec2.internal>
2021-03-15 03:34:47 +0000

    After pylint

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 '''SQLAlchemy model created for table schema'''
 from app import DB
 
+
 class Users(DB.Model):
     '''Database columns'''
     username = DB.Column(DB.String(80), primary_key=True)
     score = DB.Column(DB.Integer, nullable=False)
+
     def __repr__(self):
         return '<User %r>' % self.username

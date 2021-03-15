edfbcc8e230144fc53dd131d89ec407a3c370f81
EC2 Default User <ec2-user@ip-172-31-2-160.ec2.internal>
2021-03-15 03:55:46 +0000

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { getQueriesForElement } from "@testing-library/dom";
 import App from "./App";
+import Square from "./Square";
+import Board from "./Board";
+import Main from "./Main";
 
-test("renders learn react link", () => {
+test("Leaderboard shows when button clicked", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const ViewLeaderboardElement = screen.getByText("View Leaderboard");
+
+  //shows when button clicked
+  fireEvent.click(ViewLeaderboardElement);
+  const UsernameShows = screen.getByText("Username");
+  const ScoreShows = screen.getByText("Score");
+  expect(UsernameShows).toBeInTheDocument();
+  expect(ScoreShows).toBeInTheDocument();
+
+  //doesn't show when button toggeled
+  fireEvent.click(ViewLeaderboardElement);
+  expect(UsernameShows).not.toBeInTheDocument();
+  expect(ScoreShows).not.toBeInTheDocument();
+});
+
+test("Header shows right content", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<App />, root);
+  const { getByText, getByLabelText } = getQueriesForElement(root);
+
+  expect(root.querySelector("h1").textContent).toBe("Tic Tac Toe");
+  expect(getByText("Tic Tac Toe")).not.toBeNull();
+
 });

a87841830c50f0645bde8d946e4c439bdaf3a976
EC2 Default User <ec2-user@ip-172-31-2-160.ec2.internal>
2021-03-15 04:58:49 +0000

    After eslint

--- src/App.test.js
+++ src/App.test.js
@@ -1,36 +1,34 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { getQueriesForElement } from "@testing-library/dom";
-import App from "./App";
-import Square from "./Square";
-import Board from "./Board";
-import Main from "./Main";
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { getQueriesForElement } from '@testing-library/dom';
+import App from './App';
 
-test("Leaderboard shows when button clicked", () => {
+/* eslint-env jest */
+test('Leaderboard shows when button clicked', () => {
   render(<App />);
-  const ViewLeaderboardElement = screen.getByText("View Leaderboard");
+  const ViewLeaderboardElement = screen.getByText('View Leaderboard');
 
-  //shows when button clicked
+  // shows when button clicked
   fireEvent.click(ViewLeaderboardElement);
-  const UsernameShows = screen.getByText("Username");
-  const ScoreShows = screen.getByText("Score");
+  const UsernameShows = screen.getByText('Username');
+  const ScoreShows = screen.getByText('Score');
   expect(UsernameShows).toBeInTheDocument();
   expect(ScoreShows).toBeInTheDocument();
 
-  //doesn't show when button toggeled
+  // doesn't show when button toggeled
   fireEvent.click(ViewLeaderboardElement);
   expect(UsernameShows).not.toBeInTheDocument();
   expect(ScoreShows).not.toBeInTheDocument();
 });
 
-test("Header shows right content", () => {
-  const root = document.createElement("div");
+/* eslint-env browser */
+test('Header shows right content', () => {
+  const root = document.createElement('div');
   ReactDOM.render(<App />, root);
-  const { getByText, getByLabelText } = getQueriesForElement(root);
+  const { getByText } = getQueriesForElement(root);
 
-  expect(root.querySelector("h1").textContent).toBe("Tic Tac Toe");
-  expect(getByText("Tic Tac Toe")).not.toBeNull();
-
+  expect(root.querySelector('h1').textContent).toBe('Tic Tac Toe');
+  expect(getByText('Tic Tac Toe')).not.toBeNull();
 });
